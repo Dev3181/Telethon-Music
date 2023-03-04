89c5751d5817ae486d46afa5ff32e62215928a27
Dev3181 <126938095+Dev3181@users.noreply.github.com>
2023-03-05 05:06:06 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN",5624218847:AAH5OjoF5BfFREMhPq5H1yRtKO7xBfV0yyo)
-    STRING_SESSION = os.environ.get("STRING_SESSION", 1BVtsOGcBu4aYPM4-bW2apejWQVxJq0e-4FRgPw2qBhGMraRh2eQx_gkamcb6nyk4a0gx7Xf3-mFBDQ9Rzlt33_BBBRVzZ-hUe-oDmOKDE8wY57rPSrqt3-rQWKTvdhQenJBUtKA8IwsF_tsT1Kx3yYqdMSWPOgGC_lRY071Ig8AEtXg0VeOxD5F-el4VdF-e5YsNMBaXm4mPlhcuaAOIAr_iORK8R39z3ovL9ZmGI-2NTYN1ZIPIn7ZfD-goDLMemDy2cYxsI-ACczkniqC1RvxJdzj6ltpiBFBCvm5Vs6UemqBxOhzywq3mae7oukurUIVDUvsqWORLF2Juoryc1tcYyNA2M8w=)
+    API_ID = int(os.environ.get("API_ID", "27156263"))
+    API_HASH = os.environ.get("API_HASH", "81a4bcf394d694683072f4aa998ef09d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","5624218847:AAH5OjoF5BfFREMhPq5H1yRtKO7xBfV0yyo")
+    STRING_SESSION = os.environ.get("STRING_SESSION","1BVtsOGcBu4aYPM4-bW2apejWQVxJq0e-4FRgPw2qBhGMraRh2eQx_gkamcb6nyk4a0gx7Xf3-mFBDQ9Rzlt33_BBBRVzZ-hUe-oDmOKDE8wY57rPSrqt3-rQWKTvdhQenJBUtKA8IwsF_tsT1Kx3yYqdMSWPOgGC_lRY071Ig8AEtXg0VeOxD5F-el4VdF-e5YsNMBaXm4mPlhcuaAOIAr_iORK8R39z3ovL9ZmGI-2NTYN1ZIPIn7ZfD-goDLMemDy2cYxsI-ACczkniqC1RvxJdzj6ltpiBFBCvm5Vs6UemqBxOhzywq3mae7oukurUIVDUvsqWORLF2Juoryc1tcYyNA2M8w=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "ProtonMusic_bot")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
